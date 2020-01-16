8f03a3533c6521da3279e2c8ea00917e0a3c4bc2
clavierpaul <53355311+clavierpaul@users.noreply.github.com>
2020-01-16 20:56:01 +0000

    refactoring, indentation fixes

--- src/main/java/uk/clavier/chiplate/Renderer.java
+++ src/main/java/uk/clavier/chiplate/Renderer.java
@@ -19,7 +19,7 @@ import static org.lwjgl.system.MemoryUtil.*;
 
 enum ShaderType {
     VERTEX,
-	FRAGMENT
+    FRAGMENT
 }
 
 public class Renderer {
@@ -35,146 +35,155 @@ public class Renderer {
     }
     
     public static long createWindow(int scaleFactor) {
-        // Setup an error callback. The default implementation
-		// will print the error message in System.err.
-		GLFWErrorCallback.createPrint(System.err).set();
+        // setup an error callback
+        GLFWErrorCallback.createPrint(System.err).set();
 
-		// Initialize GLFW. Most GLFW functions will not work before doing this.
-		if ( !glfwInit() )
-			throw new IllegalStateException("Unable to initialize GLFW");
-		// Configure GLFWsupported.OpenGL30
-		glfwDefaultWindowHints(); // optional, the current wisupported.OpenGL30ndow hints are already the default
-		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // the window will be resizable
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+        // initialize GLFW
+        if ( !glfwInit() )
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        // configure GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // the window will not be resizable
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // set OpenGL version 3.3
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-		// Create the window
-		long window = glfwCreateWindow(64*scaleFactor, 32*scaleFactor, "Chiplate", NULL, NULL);
-		if ( window == NULL )
-			throw new RuntimeException("Failed to create the GLFW window");
+        // create the window
+        long window = glfwCreateWindow(64*scaleFactor, 32*scaleFactor, "Chiplate", NULL, NULL);
+        if ( window == NULL )
+            throw new RuntimeException("Failed to create the GLFW window");
 
-		// Setup a key callback. It will be called every time a key is pressed, repeated or released.
-		glfwSetKeyCallback(window, (lWindow, key, scancode, action, mods) -> {
-			if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
-				glfwSetWindowShouldClose(lWindow, true); // We will detect this in the rendering loop
-		});
+        // get the thread stack and push a new frame
+        try ( MemoryStack stack = stackPush() ) {
+            IntBuffer pWidth = stack.mallocInt(1); // int*
+            IntBuffer pHeight = stack.mallocInt(1); // int*
 
-		// Get the thread stack and push a new frame
-		try ( MemoryStack stack = stackPush() ) {
-			IntBuffer pWidth = stack.mallocInt(1); // int*
-			IntBuffer pHeight = stack.mallocInt(1); // int*
+            // get the window size passed to glfwCreateWindow
+            glfwGetWindowSize(window, pWidth, pHeight);
 
-			// Get the window size passed to glfwCreateWindow
-			glfwGetWindowSize(window, pWidth, pHeight);
+            // get the resolution of the primary monitor
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
 
-			// Get the resolution of the primary monitor
-			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+            // center the window
+            glfwSetWindowPos(
+                window,
+                (vidmode.width() - pWidth.get(0)) / 2,
+                (vidmode.height() - pHeight.get(0)) / 2
+            );
+        }
 
-			// Center the window
-			glfwSetWindowPos(
-				window,
-				(vidmode.width() - pWidth.get(0)) / 2,
-				(vidmode.height() - pHeight.get(0)) / 2
-			);
-		} // the stack frame is popped automatically
-
-		// Make the OpenGL context current
+        // make the OpenGL context current
         glfwMakeContextCurrent(window);
-		// Enable v-sync
-		glfwSwapInterval(1);
-		// Make the window visible
+        // enable v-sync
+        glfwSwapInterval(1);
+        // make the window visible
         glfwShowWindow(window);
 
         return window;
     }
 
 	private String loadShader(ShaderType type) throws IOException {
-		String toRead = "src/shaders/screen.";
+        String toRead = "src/shaders/screen.";
 
-		if (type == ShaderType.VERTEX) {
-			toRead += "vert";
-		} else {
-			toRead += "frag";
-		}
+        if (type == ShaderType.VERTEX) {
+            toRead += "vert";
+        } else {
+            toRead += "frag";
+        }
 
-		return new String (Files.readAllBytes(Paths.get(toRead)));
+        return new String (Files.readAllBytes(Paths.get(toRead)));
     }
 
-    public void init() throws IOException {
-        GL.createCapabilities();
-
-        // set the clear color
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-
-        // basic full-screen quad
+    public void createQuad() {
+        // basic full-screen quad for display
         float vertices[] = {
              1.0f,  1.0f, 0.0f,  // top right
-			 1.0f, -1.0f, 0.0f,  // bottom right
-			-1.0f, -1.0f, 0.0f,  // bottom left
+             1.0f, -1.0f, 0.0f,  // bottom right
+            -1.0f, -1.0f, 0.0f,  // bottom left
             -1.0f,  1.0f, 0.0f,  // top left
         };
-        
+
         // ebo for E F F I C I E N T R E N D E R I N G
         int indices[] = {
-			0, 1, 3, // right triangle
-			1, 2, 3  // left triangle
+            0, 1, 3, // right triangle
+            1, 2, 3  // left triangle
         };
 
         // create vao, vbo, ebo
-        vaoId = glGenVertexArrays();
+        this.vaoId = glGenVertexArrays();
         glBindVertexArray(vaoId);
 
-        vboId = glGenBuffers();
-        glBindBuffer(GL_ARRAY_BUFFER, vboId);
+        this.vboId = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, this.vboId);
         glBufferData(GL_ARRAY_BUFFER, vertices, GL_DYNAMIC_DRAW);
 
         glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
         glEnableVertexAttribArray(0);
-        
-        int eboId = glGenBuffers();
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboId);
+
+        this.eboId = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this.eboId);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_DYNAMIC_DRAW);
-        
-        // Create a base texture to update
+    }
+
+    public void createTexture() {
+        // create a base empty texture to update (needed to use the Sub function)
         ByteBuffer baseTexture = BufferUtils.createByteBuffer(64 * 32);
 
         texture = glGenTextures();
         glBindTexture(GL_TEXTURE_2D, texture);
-        
+
+        // set texture parameteres (clamp to edge, nearest neighbour scaling)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
+        // generate empty texture
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, 64, 32, 0, GL_RED, GL_UNSIGNED_BYTE, baseTexture);
+    }
 
-        // load shaders
+    public void createShaderProgram() throws IOException {
+        // load shaders from file
         String vertexShaderSource = loadShader(ShaderType.VERTEX);
-		String fragShaderSource = loadShader(ShaderType.FRAGMENT);
-        
+        String fragShaderSource = loadShader(ShaderType.FRAGMENT);
+
+        // create/compile shaders
         int vertexShader = glCreateShader(GL_VERTEX_SHADER);
         glShaderSource(vertexShader, vertexShaderSource);
         glCompileShader(vertexShader);
-        
+
         int fragShader = glCreateShader(GL_FRAGMENT_SHADER);
         glShaderSource(fragShader, fragShaderSource);
         glCompileShader(fragShader);
 
+        // link and use the shader program
         shaderProgram = glCreateProgram();
         glAttachShader(shaderProgram, vertexShader);
         glAttachShader(shaderProgram, fragShader);
-		glLinkProgram(shaderProgram);
-		
+        glLinkProgram(shaderProgram);
+        glUseProgram(shaderProgram);
+
+        // delete the shaders, not needed after link
         glDeleteShader(vertexShader);
         glDeleteShader(fragShader);  
-        
-        glUseProgram(shaderProgram);
+    }
+
+    public void init() throws IOException {
+        GL.createCapabilities();
+        this.createQuad();
+        this.createTexture();
+        this.createShaderProgram();
+
+        // set base color (probably not necessary)
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     }
 
     public void render(ByteBuffer buffer) {
+        // clear the screen (possibly unnecessary)
         glClear(GL_COLOR_BUFFER_BIT);
+
         // update texture with new data and draw it
         glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 64, 32, GL_RED, GL_UNSIGNED_BYTE, buffer);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
@@ -185,17 +194,17 @@ public class Renderer {
     }
 
     public void end() {
-        // Delete rendering objects
+        // delete rendering objects
         glDeleteBuffers(new int[] {eboId, vboId});
         glDeleteVertexArrays(vaoId);
         glDeleteTextures(texture);
         glDeleteProgram(shaderProgram);
 
-        // Free the window callbacks and destroy the window
+        // free the window callbacks and destroy the window
         glfwFreeCallbacks(window);
         glfwDestroyWindow(window);
 
-        // Terminate GLFW and free the error callback
+        // terminate GLFW and free the error callback
         glfwTerminate();
         glfwSetErrorCallback(null).free();
     }
