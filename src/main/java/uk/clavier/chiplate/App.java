00f04ce647367d84c3660fd0afe40a0d3c8be6bb
clavierpaul <53355311+clavierpaul@users.noreply.github.com>
2020-01-16 21:02:15 +0000

    removed test program

--- src/main/java/uk/clavier/chiplate/App.java
+++ src/main/java/uk/clavier/chiplate/App.java
@@ -15,10 +15,9 @@ public class App {
     private long window;
 
     private void loop() {
-        // Run the rendering loop until the user has attempted to close
-        // the window or has pressed the ESCAPE key.
+        // stop running when it's time to close
         while (!glfwWindowShouldClose(this.window)) {
-            // Cycle 9 times for roughly correct clock speed
+            // cycle 9 times for roughly correct(tm) clock speed
             for (int i = 0; i < 9; ++i) {
                 this.cpu.cycle();
             }
@@ -33,7 +32,7 @@ public class App {
         this.renderer = new Renderer(window);
         this.renderer.init();
 
-        // Setup the input handler
+        // setup the input handler
         glfwSetKeyCallback(window, (win, key, scancode, action, mods) -> {
             if (action == GLFW_RELEASE) {
                 switch (key) {
@@ -58,19 +57,11 @@ public class App {
     }
 
     public static void main(String[] args) throws IOException {
+        // read program from file and into ram
         byte[] program = Files.readAllBytes(Paths.get("programs/test_opcode.ch8"));
         Memory ram = new Memory();
-
-        byte[] test = {
-            (byte) 0x00, (byte) 0xE0,
-            (byte) 0xF5, (byte) 0x29,
-            (byte) 0xD0, (byte) 0x05,
-            (byte) 0xFA, (byte) 0x29,
-            (byte) 0xD4, (byte) 0x55,
-            (byte) 0x12, (byte) 0x0A,
-        };
-
         ram.loadProgram(program);
+
         new App().run(ram);
     }
 }
